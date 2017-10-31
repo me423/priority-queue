eb2608e6e2f5631fce089a39929f86516542a735
me423 <abduhamid.sultonov@gmail.com>
2017-10-31 11:25:42 +0300

    appendChild and removeChild methods done

--- src/node.js
+++ src/node.js
@@ -1,14 +1,37 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
-
+		if (!node) return;
+		if (this.left == null) {
+		  this.left = node;
+		}
+		else if (this.right == null) {
+		  this.right = node;
+		}
+		node.parent = this;
 	}
 
 	removeChild(node) {
-
+		if (node == null) {
+			return
+		  }
+	  
+		  if (this.left == node) {
+			this.left.parent = null;
+			this.left = null;
+		  } else if (this.right == node) {
+			this.right.parent = null;
+			this.right = null;
+		  } else {
+			throw 'incorrect child';
+		}
 	}
 
 	remove() {
