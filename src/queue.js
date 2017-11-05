2c9f958a7031de724e73f64941c0986b014a2b5c
me423 <abduhamid.sultonov@gmail.com>
2017-11-05 11:36:43 +0300

    priority-queue completed

--- src/queue.js
+++ src/queue.js
@@ -2,23 +2,30 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		this.maxSize = maxSize ? maxSize : 30;
+		this.heap = new MaxHeap();
 	}
 
 	push(data, priority) {
-
+		if (this.heap.size() == this.maxSize) {
+			throw "queue has max size";
+		}
+		this.heap.push(data, priority);
 	}
 
 	shift() {
-
+		if (this.isEmpty()) {
+			throw "queue is empty";
+		}
+		return this.heap.pop();
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		
+		return this.heap.isEmpty();
 	}
 }
 
